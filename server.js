5c15ed706263122446e35b18beb427125d227cdf
Faiz Hameed <faizftb@gmail.com>
2019-01-21 10:42:12 +0530

    upgating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-regular-53479',
     user : 'faiz',
     password : 'test123',
     database : 'smart-brain'
