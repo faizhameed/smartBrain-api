76c5ec5d7a184b2560f0c51bb1a4e5d063e19a29
Faiz Hameed <faizftb@gmail.com>
2019-01-23 22:52:38 +0530

    copied files

--- controllers/image.js
+++ controllers/image.js
@@ -3,27 +3,27 @@ const Clarifai= require ('clarifai');
 const app = new Clarifai.App({
  apiKey: '59d322a96b5f46a6ae3c325e55bb5090'
 });
-const handleApiCall = (req, res)=>{
-app.models
-.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-        .then(data =>{
-        	res.json(data);
-        })
-        .catch(err => res.status(400).json('unable to work with API'))
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
-
-const handleImage = (req,res, db)=>{
-	const { id } = req.body;
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
   db('users').where('id', '=', id)
   .increment('entries', 1)
   .returning('entries')
-  .then(entries=>{
-  	res.json(entries[0]);
+  .then(entries => {
+    res.json(entries[0]);
   })
-  .catch(err=>res.status(400).json("unable to get count"))
+  .catch(err => res.status(400).json('unable to get entries'))
 }
+
 module.exports = {
-	handleImage:handleImage,
-	handleApiCall:handleApiCall
+  handleImage,
+  handleApiCall
 }
